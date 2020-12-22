7fea4fe9196af0a983442b889af73fd9c6695a1c
jonamadk <manojadhikari@sxc.edu.np>
2020-12-22 19:11:02 +0545

    add the pronoun phrase replacement

--- chat.py
+++ chat.py
@@ -1,16 +1,16 @@
 from dictionary import responses, name, weather, bot_template, user_template
-from dictionary import time_frame, fallback_questions_answer, rules
+from dictionary import time_frame, fallback_questions_answer, rules, phrases_replacement
 import time
 import random
 import re
 
 
 class Chatbot():
-    def __init__(self,key):
+    def __init__(self,conversation_length):
         self.input_msg=""
         self.user_name=""
-        self.key_len =key
-
+        self.conversation_length = conversation_length
+  
        
        
     def take_input(self):
@@ -20,35 +20,40 @@ class Chatbot():
 
     def respond(self):
         self.take_input()
- 
 
-        for self.key_len in responses:
-       
-            time.sleep(random.choice(time_frame))
+
+        for self.conversation_length in responses:
             
+            time.sleep(random.choice(time_frame))
             self.input_msg=input(user_template.format(self.user_name)).lower()
 
             if self.input_msg not in responses:
                 response = None
                 for pattern , response in rules.items():
                     match = re.search(pattern.lower(), self.input_msg) 
-
                     if match is not None:
                         response = random.choice(response)
-
                         if '{0}' in response:
-                            self.phrase = match.group(1)
-                            ans=random.choice(rules[pattern])
-                            time.sleep(random.choice(time_frame))
-                            print(ans + self.phrase) 
-      
+                            phrase = match.group(1)
+                            for sub_key, sub_value in phrases_replacement.items():
+                                if sub_key in phrase:
+                                    replaced_phrase = re.sub(sub_key, sub_value, phrase)
+                                    time.sleep(random.choice(time_frame))
+                                    result = response.format(replaced_phrase)
+                                    print(result)
+                                    
+        
             elif self.input_msg in responses:    
                 response_text = random.choice(responses[self.input_msg])
                 time.sleep(random.choice(time_frame))
-                print(bot_template.format(response_text))
-            else:
-                print(random.choice(responses["default_fallback"]))
+                result = bot_template.format(response_text)
+                print(result)
                 
+            else:
+                result = random.choice(responses["default_fallback"])
+                print(result)
+                
+        
 
 bot = Chatbot(len(responses))
 bot.respond()
